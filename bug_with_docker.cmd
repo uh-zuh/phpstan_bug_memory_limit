73c4100c7b6772667ffd4c43f7264105b2e62434
Egor Artemenko <egorar@mail.ru>
2024-03-05 22:47:16 +0300

    Init docker state

--- bug_with_docker.cmd
+++ bug_with_docker.cmd
@@ -1,1 +1,1 @@
-docker run --rm --volume %cd%:/app --workdir /app php:8.2.14-cli ./bug.sh
+docker run --rm --volume %cd%:/app --workdir /app php:8.2.14-cli bash -c "./docker_init.sh && ./bug.sh"
--- bug_with_docker.sh
+++ bug_with_docker.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-docker run --rm --volume $(pwd):/app --workdir /app php:8.2.14-cli ./bug.sh
+docker run --rm --volume $(pwd):/app --workdir /app php:8.2.14-cli bash -c "./docker_init.sh && ./bug.sh"
